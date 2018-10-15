3560019df7aed4d88127a10619c5025784dbd1ec
Laxman Nathawat <basebytes@gmail.com>
2018-10-15 11:52:33 -0400

    This is my first commit.

---
+++ src/selections/Methods.java
@@ -0,0 +1,14 @@
+
+package selections;
+
+public class Methods {
+    public static void main(String[]args){
+        System.out.println("Sum of two numbers is: " + getSum(3, 4));
+        
+    }
+    
+    public static int getSum(int a, int b){
+        return (a+b);
+    }
+    
+}
